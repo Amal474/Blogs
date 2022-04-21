521d6ad0d05f889e08e4ce529f374dd6029af2c5
Mohammed Amal N <mohammedamal.n@gmail.com>
2022-04-21 06:29:10 +0300

    add later

---
+++ purchase_auto_date/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Base Automation Sample",
+    'version': '15.0.1.0',
+    'summary': """Automatic update of notes in purchase""",
+    'description': """
+        Purchase Base Automation Sample
+            -Automatic update of notes in purchase
+    """,
+    'author': "Mohammed Amal N",
+    'category': 'Test',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base_automation', 'purchase'],
+    'data': [
+    ],
+    'application': True,
+}
